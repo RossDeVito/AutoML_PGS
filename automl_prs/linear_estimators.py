d3075c9d3fccd30576d1a225e4576c0e9e6f0ab0
RossDeVito <rdevito@ucsd.edu>
2024-04-04 19:16:15 -0700

    Add row partitioning to linear models with n=3

--- automl_prs/linear_estimators.py
+++ automl_prs/linear_estimators.py
@@ -4,18 +4,80 @@ import time
 import logging
 from pprint import pprint
 
+import numpy as np
+import pandas as pd
+import polars as pl
 from flaml.automl.model import SKLearnEstimator
 from flaml import tune
 from sklearn import linear_model
-from sklearn import pipeline
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.base import clone
+from sklearn.base import BaseEstimator, RegressorMixin
+from tqdm import tqdm
 
 
 logger = logging.getLogger(__name__)
 
 
+def subset_data(data, start, end, axis=0):
+	if isinstance(data, (pd.DataFrame, pd.Series)):
+		return data.iloc[start:end] if axis == 0 else data.iloc[:, start:end]	# type: ignore
+	elif isinstance(data, pl.DataFrame):
+		return data.filter(pl.col("*").slice(start, end))
+	else:
+		return data[start:end] if axis == 0 else data[:, start:end]
+	
+
+class PartitionedEnsembleRows(BaseEstimator, RegressorMixin):
+	def __init__(self, estimator, n_partitions=3, verbose=0, **kwargs):
+		self.estimator = estimator
+		self.n_partitions = n_partitions
+		self.verbose = verbose
+		self.kwargs = kwargs
+		self.models = []
+
+	def fit(self, X, y):
+		indices = np.arange(len(y))
+		np.random.shuffle(indices)
+
+		partition_size = len(y) // self.n_partitions
+
+		for i in tqdm(
+			range(self.n_partitions),
+			desc='Fitting on sample partitions',
+			total=self.n_partitions,
+			ncols=100,
+			disable=(self.verbose == 0)
+		):
+			start_idx = i * partition_size
+			end_idx = (i + 1) * partition_size if i != self.n_partitions - 1 else len(y)
+			X_subset = subset_data(X, start_idx, end_idx)
+			y_subset = subset_data(y, start_idx, end_idx)
+
+			model = clone(self.estimator)
+			model.set_params(**self.kwargs)
+			model.fit(X_subset, y_subset)
+			self.models.append(model)
+
+		return self
+
+	def predict(self, X):
+		predictions = np.zeros(len(X))
+
+		for model in tqdm(
+			self.models,
+			desc='Predicting with all models',
+			total=len(self.models),
+			ncols=100,
+			disable=(self.verbose == 0)
+		):
+			predictions += model.predict(X)
+
+		return predictions / self.n_partitions
+
+
 class ElasticNetEstimatorPRS(SKLearnEstimator):
-	"""Elastic net estimator. """
+	"""Elastic net estimator using n_partitions paritions."""
 
 	@classmethod
 	def search_space(cls, data_size, task):
@@ -47,11 +109,17 @@ class ElasticNetEstimatorPRS(SKLearnEstimator):
 	def __init__(
 			self,
 			task="regression",
+			n_partitions=3,
 			n_jobs=None,
 			scale=True,
 			**config
 		):
-		super().__init__(task, **config)
+		super().__init__(
+			task,
+			n_partitions=n_partitions,
+			estimator=linear_model.ElasticNet(),
+			**config
+		)
 		
 		if task != "regression":
 			raise ValueError(
@@ -65,7 +133,7 @@ class ElasticNetEstimatorPRS(SKLearnEstimator):
 			self.scaler = None
 			self.scaler_fit = True
 		
-		self.estimator_class = linear_model.ElasticNet
+		self.estimator_class = PartitionedEnsembleRows
 
 	def _preprocess(self, X):
 		"""Preprocess data, including scaling."""
